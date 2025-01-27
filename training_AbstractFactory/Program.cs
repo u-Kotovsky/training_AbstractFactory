c7aa5964ada8413caa9c804a354d1bfb7b65e1a0
u-Kotovsky <kotovsky.2021@gmail.com>
2025-01-27 20:19:16 +0300

    Add project files.

---
+++ training_AbstractFactory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace training_AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
